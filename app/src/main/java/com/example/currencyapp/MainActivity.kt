9e1ada2921b162b5ed85d885b5788f032687c01f
Alex <sagtaret@gmail.com>
2021-12-11 11:58:19 +0300

    Added a Currency Screen

--- app/src/main/java/com/example/currencyapp/MainActivity.kt
+++ app/src/main/java/com/example/currencyapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.currencyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.currencyapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+        bind.btnEnter.setOnClickListener {
+
+        }
     }
 }
