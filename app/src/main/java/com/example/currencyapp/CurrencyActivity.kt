41ced366ab608495130ff82a250ad596fa4c0e52
Alex <sagtaret@gmail.com>
2021-12-11 14:47:25 +0300

    The App is Done

--- app/src/main/java/com/example/currencyapp/CurrencyActivity.kt
+++ app/src/main/java/com/example/currencyapp/CurrencyActivity.kt
@@ -2,10 +2,35 @@ package com.example.currencyapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.TableRow
+import android.widget.TextView
+import androidx.core.widget.doOnTextChanged
+import com.example.currencyapp.consts.Consts
+import com.example.currencyapp.databinding.ActivityCurrencyBinding
 
 class CurrencyActivity : AppCompatActivity() {
+
+    lateinit var bind: ActivityCurrencyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_currency)
+        bind = ActivityCurrencyBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+
+
+
+        bind.apply {
+
+
+            btnConvert.setOnClickListener{
+
+
+            var a = etCurrData.text.toString().toDouble()
+
+          textView2.text = (a * Consts.RUB).toString()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/currencyapp/consts/Consts.kt
+++ app/src/main/java/com/example/currencyapp/consts/Consts.kt
@@ -1,6 +1,8 @@
 package com.example.currencyapp.consts
 
+import java.math.BigDecimal
+
 object Consts {
-
-
+    const val RUB= 73.44
+    const val USD = 0.7
 }
